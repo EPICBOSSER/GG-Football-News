1270410d541d5175b87354f509919a9c19ed5f90
EPICBOSSER <ggbejewoh@gmail.com>
2016-07-22 12:55:33 +0100

    This Is My First Project -Zavala

---
+++ Script.js
@@ -0,0 +1,18 @@
+alert ("Welcome To Our Website!")
+
+var Entry_Message = ("Here You Can Get The Latest Info On Football From All Around The World!")
+
+alert (Entry_Message);
+
+var Online_Users = 127;
+var Offline_Users = 43;
+var Total_Users = Online_Users + Offline_Users;
+
+alert ("Online Users = 127")
+
+
+
+
+
+
+
